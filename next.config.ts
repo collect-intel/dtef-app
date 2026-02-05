192075bfacd0f3e1cdaab733a8e0b3dd17ad74c3
evanhadfield <147835824+evanhadfield@users.noreply.github.com>
2026-02-04 17:29:43 -0800

    Fix build OOM: skip Sentry plugin when env vars missing, use env-based NODE_OPTIONS

--- next.config.ts
+++ next.config.ts
@@ -44,47 +44,22 @@ const nextConfig: NextConfig = {
   },
 };
 
-// Wrap with bundle analyzer first, then Sentry
+// Wrap with bundle analyzer first, then conditionally with Sentry
 const configWithAnalyzer = withBundleAnalyzer(nextConfig);
 
-// Sentry configuration options
-export default withSentryConfig(configWithAnalyzer, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+const hasSentry = !!(process.env.SENTRY_ORG && process.env.SENTRY_PROJECT);
 
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the Sentry DSN provided to the browser is the same one used for this configuration.
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  sourcemaps: {
-    disable: true,
-  },
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+// Only apply Sentry build plugin when credentials are available
+export default hasSentry
+  ? withSentryConfig(configWithAnalyzer, {
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      silent: !process.env.CI,
+      widenClientFileUpload: true,
+      reactComponentAnnotation: { enabled: true },
+      tunnelRoute: "/monitoring",
+      sourcemaps: { disable: true },
+      disableLogger: true,
+      automaticVercelMonitors: true,
+    })
+  : configWithAnalyzer;
